55859280d553c3d28b81fc24f9c6528634d55f5f
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 11:35:26 -0400

    switch one if statement

--- bob.py
+++ bob.py
@@ -1,10 +1,10 @@
 def response(hey_bob):
-    if hey_bob.isupper():
-        bob =  "Whoa, chill out!"
-    elif hey_bob.endswith("?"):
+    if hey_bob.endswith("?"):
         bob = "Sure."
     elif hey_bob.endswith("?") and hey_bob.isupper():
         bob = "Calm down, I know what I'm doing!"
+    elif hey_bob.isupper():
+        bob =  "Whoa, chill out!"
     elif hey_bob == "" or hey_bob.isspace():
         bob = "Fine. Be that way!"
     else:
