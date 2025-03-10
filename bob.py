eb286b72d99d8b920cfcd0bc272fc0ab043aaacf
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 11:39:58 -0400

    switched order of if statement

--- bob.py
+++ bob.py
@@ -1,8 +1,8 @@
 def response(hey_bob):
-    if hey_bob.endswith("?") and not hey_bob.isupper:
-        bob = "Sure."
-    elif hey_bob.endswith("?") and hey_bob.isupper():
+    if hey_bob.endswith("?") and hey_bob.isupper():
         bob = "Calm down, I know what I'm doing!"
+    elif hey_bob.endswith("?"):
+        bob = "Sure."
     elif hey_bob.isupper():
         bob =  "Whoa, chill out!"
     elif hey_bob == "" or hey_bob.isspace():
