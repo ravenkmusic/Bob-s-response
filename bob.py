d1a47310a7963cc782259f7df18c59cbf624b7ed
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 11:10:21 -0400

    added keywords

--- bob.py
+++ bob.py
@@ -1,7 +1,7 @@
 def response(hey_bob):
     if hey_bob.endswith("?") and hey_bob.isupper:
         bob = "Calm down, I know what I'm doing!"
-    elif hey_bob.endswith("?"):
+    elif hey_bob.endswith("?") and not hey_bob.isupper:
         bob = "Sure."
     if hey_bob.isupper:
         bob =  "Whoa, chill out!"
