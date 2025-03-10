d96e5ed57f6ee9a0d8ac552d70df88785b25de09
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 10:36:59 -0400

    added bob variable

--- bob.py
+++ bob.py
@@ -1,11 +1,13 @@
 def response(hey_bob):
     if hey_bob.endswith("?") and hey_bob.isupper:
-        return "Calm down, I know what I'm doing!"
+        bob = "Calm down, I know what I'm doing!"
     elif hey_bob.endswith("?"):
-        return "Sure."
-    if hey_bob.isupper:
-        return  "Whoa, chill out!"
-    if hey_bob == "":
-        return "Fine. Be that way!"
+        bob = "Sure."
+    elif hey_bob.isupper:
+        bob =  "Whoa, chill out!"
+    elif hey_bob == "":
+        bob = "Fine. Be that way!"
     else:
-        return "Whatever."
+        bob = "Whatever."
+        
+    return bob
