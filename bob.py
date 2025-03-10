b9305578047ded613eeedd208624a804a2087137
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 11:28:51 -0400

    changed ifs to elifs

--- bob.py
+++ bob.py
@@ -1,11 +1,11 @@
 def response(hey_bob):
     if hey_bob.isupper():
         bob =  "Whoa, chill out!"
-    if hey_bob.endswith("?"):
+    elif hey_bob.endswith("?"):
         bob = "Sure."
-    if hey_bob.endswith("?") and hey_bob.isupper():
+    elif hey_bob.endswith("?") and hey_bob.isupper():
         bob = "Calm down, I know what I'm doing!"
-    if hey_bob == "":
+    elif hey_bob == "":
         bob = "Fine. Be that way!"
     else:
         bob = "Whatever."
