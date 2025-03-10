b6e43c8fdb0e63df26c4f36cd00c92e6a279dda1
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 11:18:49 -0400

    edited if statement

--- bob.py
+++ bob.py
@@ -3,7 +3,7 @@ def response(hey_bob):
         bob =  "Whoa, chill out!"
     if hey_bob.endswith("?") and hey_bob.isupper():
         bob = "Calm down, I know what I'm doing!"
-    elif hey_bob.endswith("?") and not hey_bob.isupper():
+    elif hey_bob.endswith("?"):
         bob = "Sure."
     if hey_bob == "":
         bob = "Fine. Be that way!"
