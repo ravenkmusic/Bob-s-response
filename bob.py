14b85e8dbd54e078a12b99309e44ae61d8a7dd76
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 11:12:00 -0400

    rearranged if statements

--- bob.py
+++ bob.py
@@ -1,10 +1,10 @@
 def response(hey_bob):
+    if hey_bob.isupper:
+        bob =  "Whoa, chill out!"
     if hey_bob.endswith("?") and hey_bob.isupper:
         bob = "Calm down, I know what I'm doing!"
     elif hey_bob.endswith("?") and not hey_bob.isupper:
         bob = "Sure."
-    if hey_bob.isupper:
-        bob =  "Whoa, chill out!"
     if hey_bob == "":
         bob = "Fine. Be that way!"
     else:
