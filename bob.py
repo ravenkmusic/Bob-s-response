f2a39bca3e9a530aaede46277334b304772c53e5
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 11:17:24 -0400

    fixed typo in isupper method

--- bob.py
+++ bob.py
@@ -1,9 +1,9 @@
 def response(hey_bob):
-    if hey_bob.isupper and hey_bob.endswith("!"):
+    if hey_bob.isupper():
         bob =  "Whoa, chill out!"
-    if hey_bob.endswith("?") and hey_bob.isupper:
+    if hey_bob.endswith("?") and hey_bob.isupper():
         bob = "Calm down, I know what I'm doing!"
-    elif hey_bob.endswith("?") and not hey_bob.isupper:
+    elif hey_bob.endswith("?") and not hey_bob.isupper():
         bob = "Sure."
     if hey_bob == "":
         bob = "Fine. Be that way!"
