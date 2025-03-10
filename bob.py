4dc0a18cb55580a029f8ffc65083538eb5772fb0
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-10 10:23:35 -0400

    first basic test

--- bob.py
+++ bob.py
@@ -1,2 +1,11 @@
 def response(hey_bob):
-    pass
+    if hey_bob.endswith("?"):
+        return "Sure."
+    elif hey_bob.isupper:
+            return "Calm down, I know what I'm doing!"
+    if hey_bob.isupper:
+        return "Whoa, chill out!" 
+    if hey_bob == "":
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
